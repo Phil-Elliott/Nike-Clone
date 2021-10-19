8e8e57ebeb19857daa25b4854aa3ca32bdb267eb
Phil_Elliott <phillippelliott@gmail.com>
2021-10-19 14:13:44 -0400

    Fixed Scroll and dropdown for Help link

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const slider = document.querySelector('.slider'); 
+const middle = document.querySelector('.middle');  
 
+
+
+// Fixed position for the middle header
+let sticky = middle.offsetTop;
+let previousScroll = 0; 
+
+document.addEventListener('scroll', () => {
+	if (window.pageYOffset > 148) {
+		middle.classList.add("sticky")
+		console.log(document.height)
+	} else {
+		middle.classList.remove("sticky")
+	}
+})
+
+// Slider function for the 3rd header
 width = 0;
 
 const moveRight = () => {
@@ -15,4 +32,19 @@ setInterval(moveRight,10000)
 
 
 
+
+
+
+/*
+1) Creat a scrolling navbar 
+	maybe have set the background to white 
+	have it set off when scrolling up 
+	have it only happen at a certain point of the page
+
+	add a sticky class 
+	insert it to the html navbar when it reaches a certain position on the page 
+
+	OffsetY greater than 148
+
+*/
 
